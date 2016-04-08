816bc1e91cef734ab78eea8b5c063affd231d7cc
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 15:26:51 +0200

    Indentation

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO" > data/produits.csv
-grep '<strong>Produit</strong>' html/* | sed 's/<li>/;/g' | sed 's/<li>/;/g'  | awk -F ';' 'BEGIN{OFS=";"}  {if ( $4 !~ /Statut FR/ ) $4=";"$4 ; print $0 ;}' | sed 's/<strong>[^<]*<.strong>//g' |  sed 's/<[^>]*>//g' | sed 's/Description.*//' | sed 's/[^;]*: *//g'  | awk -F ';' '{gsub(" - ", ";", $6) ; print $5";"$4";"$7";"$8";"$2";"$6}' | awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /[0-9][A-Z][0-9]*/ ) $9 = ";;"$9 ; if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";;"$10 ; print $0 }' | sed 's/[,"]//g' | sed 's/;/","/g' | sed 's/$/"/' | sed 's/^/"/' >> data/produits.csv
+grep '<strong>Produit</strong>' html/* | 
+	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | 
+	awk -F ';' 'BEGIN{OFS=";"}  {if ( $4 !~ /Statut FR/ ) $4=";"$4 ; print $0 ;}' | 
+	sed 's/<strong>[^<]*<.strong>//g' |  sed 's/<[^>]*>//g' | sed 's/Description.*//' | sed 's/[^;]*: *//g'  | 
+	awk -F ';' '{gsub(" - ", ";", $6) ; print $5";"$4";"$7";"$8";"$2";"$6}' | 
+	awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /[0-9][A-Z][0-9]*/ ) $9 = ";;"$9 ; if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";"$10 ; print $0 }' | 
+	sed 's/[,"]//g' | sed 's/;/","/g' | sed 's/$/"/' | sed 's/^/"/' >> data/produits.csv
 
