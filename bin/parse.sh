168d066e7ad2e6ab17f9cd6c0e9d8299e5586d26
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 18:38:17 +0200

    Divise les fichiers afin de pouvoir afficher les vins

--- bin/parse.sh
+++ bin/parse.sh
@@ -10,7 +10,15 @@ grep '<strong>Produit</strong>' html/* |
         awk -F ';' 'BEGIN {OFS=";"} { if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";;"$10 ; if ( $11 ~ /[0-9][A-Z][0-9]*/ ) $11 = ";"$11 ; print $0 }' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /^Sec/ ) { $10 = $9 ; $9 = "" } if ( $10 ~ /^(Dép|Grand|Premier|Régio|Zone)/ ) { $11 = $10 ; $10 = "" } print $0 }' | 
         awk -F ';' 'BEGIN {OFS=";"} { $13=$1 ; $1=""; print $0}' | sed 's/^;//' | 
-        sed 's/ . Appellation d.origine protégée//' | sed 's/ . Indication géographique protégée//' | sed 's/ . Indication géographique//' |
-	sed 's/ - Appellation d.origine contrôlée//' | sed 's/ - Appellation d.origine réglementée//' | sed 's/ - Label rouge//' | 
 	sed 's/[,"]//g' | sed 's/;/,/g' >> data/produits.csv
 
+echo "Statut,Appellation,Produit,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,id site" > data/vins.csv
+cat data/produits.csv | grep ',Vins,' | sed 's/,Vins,/,/' |
+        sed 's/ *([^)]*)//' | 
+        sed 's/ . Appellation d.origine protégée//' | sed 's/ . Indication géographique protégée//' | sed 's/ . Indication géographique//' |
+	sed 's/ - Appellation d.origine contrôlée//' | sed 's/ - Appellation d.origine réglementée//' | sed 's/ - Label rouge//' |
+	sed 's/^,AOC,/AOC,/' | sed 's/^AOP,AOC,/AOC,/' | sed 's/^IGP,,/IGP,/' |
+	awk -F ',' '{print $1","$2","$4","$5","$6","$7","$8","$9}' |
+	cat >> data/vins.csv
+
+cat data/produits.csv | grep -v ',Vins,'  > data/nonvins.csv
