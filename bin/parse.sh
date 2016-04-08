ae6e841407de78cfe037c54a25d066b809f4d2b7
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 18:40:53 +0200

    Réintégration des URL + entete des vins en accord avec les données

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Statut CE,Statut FR,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,id site" > data/produits.csv
+echo "Statut CE,Statut FR,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,url site" > data/produits.csv
 grep '<strong>Produit</strong>' html/* | 
 	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | sed 's/:/;/' | sed 's/^html.//' |
 	awk -F ';' 'BEGIN{OFS=";"}  {if ( $5 !~ /Statut FR/ ) $5=";"$5 ; print $0 ;}' | 
@@ -9,10 +9,10 @@ grep '<strong>Produit</strong>' html/* |
 	awk -F ';' '{gsub(/ - ?/, ";", $7) ; print $1";"$6";"$5";"$8";"$9";"$3";"$7}' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";;"$10 ; if ( $11 ~ /[0-9][A-Z][0-9]*/ ) $11 = ";"$11 ; print $0 }' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /^Sec/ ) { $10 = $9 ; $9 = "" } if ( $10 ~ /^(Dép|Grand|Premier|Régio|Zone)/ ) { $11 = $10 ; $10 = "" } print $0 }' | 
-        awk -F ';' 'BEGIN {OFS=";"} { $13=$1 ; $1=""; print $0}' | sed 's/^;//' | 
+        awk -F ';' 'BEGIN {OFS=";"} { $13="http://www.inao.gouv.fr/produit/"$1 ; $1=""; print $0}' | sed 's/^;//' | 
 	sed 's/[,"]//g' | sed 's/;/,/g' >> data/produits.csv
 
-echo "Statut,Appellation,Produit,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,id site" > data/vins.csv
+echo "Statut,Appellation,Produit,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO" > data/vins.csv
 cat data/produits.csv | grep ',Vins,' | sed 's/,Vins,/,/' |
         sed 's/ *([^)]*)//' | 
         sed 's/ . Appellation d.origine protégée//' | sed 's/ . Indication géographique protégée//' | sed 's/ . Indication géographique//' |
