240a432228dbb71f198139b5eeb0fbea1daec178
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 18:00:01 +0200

    Correction bug sur le statut EU

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,id site" > data/produits.csv
+echo "Statut CE,Statut FR,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,id site" > data/produits.csv
 grep '<strong>Produit</strong>' html/* | 
 	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | sed 's/:/;/' | sed 's/^html.//' |
 	awk -F ';' 'BEGIN{OFS=";"}  {if ( $5 !~ /Statut FR/ ) $5=";"$5 ; print $0 ;}' | 
+	awk -F ';' 'BEGIN{OFS=";"}  {if ( $6 !~ /Statut CE/ ) $6=";"$6 ; print $0 ;}' | 
 	sed 's/<strong>[^<]*<.strong>//g' |  sed 's/<[^>]*>//g' | sed -r 's/(Description|Historique|Chiffres-clés).*//' | sed 's/[^;]*: *//g'  | 
 	awk -F ';' '{gsub(/ - ?/, ";", $7) ; print $1";"$6";"$5";"$8";"$9";"$3";"$7}' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";;"$10 ; if ( $11 ~ /[0-9][A-Z][0-9]*/ ) $11 = ";"$11 ; print $0 }' | 
