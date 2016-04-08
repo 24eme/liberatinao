a155689d286e770822ccb0ee5b6804c14fac35b3
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 13:06:03 +0200

    Télécharge les fichiers utiles pour l'extraction des données

---
+++ bin/download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir -p tmp html
+curl -s http://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit | grep '^</option><option ' | grep dep | sed 's/.*value="//' | sed 's/".*//' | while read dep ; do 
+        for (( i = 0 ; i < 100 ; i++)) ; do
+	curl -s "http://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit?actimage_design_product_type%5BsearchType%5D=product&actimage_design_product_type%5Bdepartement%5D="$dep"&page="$i > tmp/$$.html
+        cat tmp/$$.html | sed 's|</li>|</li>\n|g' | grep '/produit/' | sed 's/.*href=.//' | sed 's/".*//' 
+        if ! grep 'a class="next' tmp/$$.html > /dev/null ; then
+		break;
+	fi
+        done
+done > tmp/produit_inao.list
+
+rm tmp/$$.html
+
+sort -u tmp/produit_inao.list > tmp/produit_inao.uniq.list
+
+wget -nc -i tmp/produit_inao.uniq.list -P html/
+
