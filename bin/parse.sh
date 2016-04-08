b9467acb80fd066acbaa37515feeb036569e19ca
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 16:45:16 +0200

    Ouvre la possibilité de conserver l'id utilisé sur le site de l'INAO

--- bin/parse.sh
+++ bin/parse.sh
@@ -2,11 +2,11 @@
 
 echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO" > data/produits.csv
 grep '<strong>Produit</strong>' html/* | 
-	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | 
-	awk -F ';' 'BEGIN{OFS=";"}  {if ( $4 !~ /Statut FR/ ) $4=";"$4 ; print $0 ;}' | 
+	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | sed 's/:/;/' | sed 's/^html.//' |
+	awk -F ';' 'BEGIN{OFS=";"}  {if ( $5 !~ /Statut FR/ ) $5=";"$5 ; print $0 ;}' | 
 	sed 's/<strong>[^<]*<.strong>//g' |  sed 's/<[^>]*>//g' | sed -r 's/(Description|Historique|Chiffres-clés).*//' | sed 's/[^;]*: *//g'  | 
-	awk -F ';' '{gsub(" - ?", ";", $6) ; print $5";"$4";"$7";"$8";"$2";"$6}' | 
-	awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /[0-9][A-Z][0-9]*/ ) $9 = ";;"$9 ; if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";"$10 ; print $0 }' | 
+	awk -F ';' '{gsub(/ - ?/, ";", $7) ; print $6";"$5";"$8";"$9";"$3";"$7}' | 
+        awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /[0-9][A-Z][0-9]*/ ) $9 = ";;"$9 ; if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";"$10 ; print $0 }' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $8 ~ /^Sec/ ) { $9 = $8 ; $8 = "" } if ( $9 ~ /^(Dép|Grand|Premier|Régio|Zone)/ ) { $10 = $9 ; $9 = "" } print $0 }' | 
 	sed 's/[,"]//g' | sed 's/;/","/g' | sed 's/$/"/' | sed 's/^/"/' >> data/produits.csv
 
