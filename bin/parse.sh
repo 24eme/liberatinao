8ecd100955c0691400bca94cd9a20fe6eb42a3ef
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 17:37:32 +0200

    Réduction de la taille du fichier CSV (pour pouvoir être afficié dans l'apperçu github)

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,URL Source" > data/produits.csv
+echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,id site" > data/produits.csv
 grep '<strong>Produit</strong>' html/* | 
 	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | sed 's/:/;/' | sed 's/^html.//' |
 	awk -F ';' 'BEGIN{OFS=";"}  {if ( $5 !~ /Statut FR/ ) $5=";"$5 ; print $0 ;}' | 
@@ -8,6 +8,6 @@ grep '<strong>Produit</strong>' html/* |
 	awk -F ';' '{gsub(/ - ?/, ";", $7) ; print $1";"$6";"$5";"$8";"$9";"$3";"$7}' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";;"$10 ; if ( $11 ~ /[0-9][A-Z][0-9]*/ ) $11 = ";"$11 ; print $0 }' | 
         awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /^Sec/ ) { $10 = $9 ; $9 = "" } if ( $10 ~ /^(Dép|Grand|Premier|Régio|Zone)/ ) { $11 = $10 ; $10 = "" } print $0 }' | 
-        awk -F ';' 'BEGIN {OFS=";"} { $13="http://www.inao.gouv.fr/produit/"$1 ; $1=""; print $0}' | sed 's/^;//' | 
-	sed 's/[,"]//g' | sed 's/;/","/g' | sed 's/$/"/' | sed 's/^/"/' >> data/produits.csv
+        awk -F ';' 'BEGIN {OFS=";"} { $13=$1 ; $1=""; print $0}' | sed 's/^;//' | 
+	sed 's/[,"]//g' | sed 's/;/,/g' >> data/produits.csv
 
