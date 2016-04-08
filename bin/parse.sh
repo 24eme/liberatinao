24f37ce7aa40e5fc996b1b178eef1b4937011fa9
Tangui Morlier <tangui@tangui.eu.org>
2016-04-08 17:23:56 +0200

    Ajout de l'URL source de l'information

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO" > data/produits.csv
+echo "Statut FR,Statut CE,Appellation,Dénomination,Produit,Catégorie générale,Sous Catégorie,Couleur,Sucrosité,Mention,Code INAO,URL Source" > data/produits.csv
 grep '<strong>Produit</strong>' html/* | 
 	sed 's/<li>/;/g' | sed 's/<li>/;/g'  | sed 's/:/;/' | sed 's/^html.//' |
 	awk -F ';' 'BEGIN{OFS=";"}  {if ( $5 !~ /Statut FR/ ) $5=";"$5 ; print $0 ;}' | 
 	sed 's/<strong>[^<]*<.strong>//g' |  sed 's/<[^>]*>//g' | sed -r 's/(Description|Historique|Chiffres-clés).*//' | sed 's/[^;]*: *//g'  | 
-	awk -F ';' '{gsub(/ - ?/, ";", $7) ; print $6";"$5";"$8";"$9";"$3";"$7}' | 
-        awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /[0-9][A-Z][0-9]*/ ) $9 = ";;"$9 ; if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";"$10 ; print $0 }' | 
-        awk -F ';' 'BEGIN {OFS=";"} { if ( $8 ~ /^Sec/ ) { $9 = $8 ; $8 = "" } if ( $9 ~ /^(Dép|Grand|Premier|Régio|Zone)/ ) { $10 = $9 ; $9 = "" } print $0 }' | 
+	awk -F ';' '{gsub(/ - ?/, ";", $7) ; print $1";"$6";"$5";"$8";"$9";"$3";"$7}' | 
+        awk -F ';' 'BEGIN {OFS=";"} { if ( $10 ~ /[0-9][A-Z][0-9]*/ ) $10 = ";;"$10 ; if ( $11 ~ /[0-9][A-Z][0-9]*/ ) $11 = ";"$11 ; print $0 }' | 
+        awk -F ';' 'BEGIN {OFS=";"} { if ( $9 ~ /^Sec/ ) { $10 = $9 ; $9 = "" } if ( $10 ~ /^(Dép|Grand|Premier|Régio|Zone)/ ) { $11 = $10 ; $10 = "" } print $0 }' | 
+        awk -F ';' 'BEGIN {OFS=";"} { $13="http://www.inao.gouv.fr/produit/"$1 ; $1=""; print $0}' | sed 's/^;//' | 
 	sed 's/[,"]//g' | sed 's/;/","/g' | sed 's/$/"/' | sed 's/^/"/' >> data/produits.csv
 
