d0402c9913259494e4634665c70c19d3d7bc930f
Tangui Morlier <tangui@tangui.eu.org>
2018-03-20 13:47:56 +0100

    Update parsing

--- bin/download.sh
+++ bin/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p tmp html
-curl -s http://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit | grep '^</option><option ' | grep dep | sed 's/.*value="//' | sed 's/".*//' | while read dep ; do 
+curl -s https://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit | sed 's|\(</*option\)|\n\1|g' | grep '^<option' | grep dep | sed 's/.*value="//' | sed 's/".*//' | while read dep ; do 
         for (( i = 0 ; i < 100 ; i++)) ; do
-	curl -s "http://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit?actimage_design_product_type%5BsearchType%5D=product&actimage_design_product_type%5Bdepartement%5D="$dep"&page="$i > tmp/$$.html
+	curl -s "https://www.inao.gouv.fr/Espace-professionnel-et-outils/Rechercher-un-produit?actimage_design_product_type%5BsearchType%5D=product&actimage_design_product_type%5Bdepartement%5D="$dep"&page="$i > tmp/$$.html
         cat tmp/$$.html | sed 's|</li>|</li>\n|g' | grep '/produit/' | sed 's/.*href=.//' | sed 's/".*//' 
         if ! grep 'a class="next' tmp/$$.html > /dev/null ; then
 		break;
